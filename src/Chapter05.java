22da9f277522ecf5955d766c546e3c37b0bcf2f8
monqcq <takuyaaaan23@gmail.com>
2021-06-26 14:36:27 +0900

    [add]５章の演習問題

---
+++ src/Chapter05.java
@@ -0,0 +1,17 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long num1;
+		double num2;
+		boolean boolean1;
+		num1 = 1234;
+		num2 = 1234.5;
+		boolean1 = true;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(boolean1);
+		int[] array1 = { 1, 2, 3, 4, 5};
+		int int2 = array1[2];
+		System.out.println(int2);
+	}
+}
