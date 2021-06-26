e5476882a529ae5723fe44fbaf878c7e3565f720
monqcq <takuyaaaan23@gmail.com>
2021-06-26 14:10:43 +0900

    [add]４章の確認問題

---
+++ src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name;
+		name = "Takahashi Takuya";
+		System.out.println(name);
+		name = "monqcq";
+		System.out.println(name);
+		int num1;
+		num1 = 7;
+		System.out.println(num1);
+	}
+}
